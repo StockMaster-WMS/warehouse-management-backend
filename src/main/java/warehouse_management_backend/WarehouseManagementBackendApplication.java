13480cc167df99ee9b8066bbbd74314db978122c
Trần Trân Lộc <tran26122003@gmail.com>
2026-02-12 22:52:10 +0700

    Init Project

---
+++ src/main/java/warehouse_management_backend/WarehouseManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package warehouse_management_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseManagementBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/warehouse_management_backend/WarehouseManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package warehouse_management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
